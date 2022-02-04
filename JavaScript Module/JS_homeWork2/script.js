62dfe47a9690a1a930b04d4c4b725349e1ab54ba
Zarovnii Serghei <Zarovniyserhiy1991@gmail.com>
2022-02-04 12:24:55 +0100

    JS homeWork 2 is done

--- JavaScript Module/JS_homeWork2/script.js
+++ JavaScript Module/JS_homeWork2/script.js
@@ -50,3 +50,53 @@
 // } else {
 //     console.log('False')
 // }
+
+// - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день.
+
+
+// let day = prompt('Enter day of the week');
+//  switch (day) {
+//      case '1' :
+//          console.log('Its Monday!!! Learning Java Script')
+//          break
+//      case '2':
+//          console.log('Its Tuesday!!! Go to work')
+//          break
+//      case '3':
+//          console.log('Its Wednesday!!! Learning HTML')
+//          break
+//      case '4':
+//          console.log('Its Thursday!!! Go to girlfriend')
+//          break
+//      case '5':
+//          console.log('Its Friday!!! Learning React JS')
+//          break
+//      case '6':
+//          console.log('Its Saturday!!! Go to the city')
+//          break
+//      case '7':
+//          console.log('Its Sunday Go to central park')
+//      default:
+//          console.error('You enter wrong number')
+//  }
+
+// - Потрібно визначити Високосний рік чи ні. Високосні роки діляться націло на 4.
+
+// let year = prompt('Enter number of the year');
+//
+// if(year % 4 === 0 ){
+//     console.log('Its a Leap Year')
+// } else {
+//     console.log('Its a normal Year')
+// }
+
+// - Використовуючи конструкцію if..else, напишіть код, який запитуватиме: „Яка «офіційна» назва JavaScript?“
+// Якщо користувач вводить «ECMAScript», то показати: «Правильно!», інакше – відобразити: «Не знаєте? ECMAScript!
+
+// let answer = prompt('Witch the official name of JavaScript???')
+//
+// if (answer === 'ECMAScript') {
+//     console.log('Correct')
+// } else {
+//     console.log('You dont know??? Its ECMAScript')
+// }
