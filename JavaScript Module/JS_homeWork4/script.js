3962f8d77a5428d265c60f5959caa048186fbd41
Zarovnii Serghei <Zarovniyserhiy1991@gmail.com>
2022-02-08 13:18:27 +0100

    Homework is done

--- JavaScript Module/JS_homeWork4/script.js
+++ JavaScript Module/JS_homeWork4/script.js
@@ -44,6 +44,73 @@
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
 //     Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
+// function ListCreator (text,num) {
+// document.write(`<ul>`)
+//     for (let i = 0; i < num; i++) {
+//         document.write(`<li>${text}</li>`);
+//     }
+// document.write(`</ul>`)
+// };
+//
+// ListCreator('JS',100)
+
+
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+// let array = ['Hello',152,'Java',569,'MyDream',56699,false];
+//
+// function listCreator (arr) {
+//     document.write(`<ul>`)
+//     for (const arrElement of arr) {
+//         document.write(`<li> ${arrElement} </li>`)
+//     }
+//     document.write(`</ul>`)
+// }
+// listCreator(array)
+
+
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+// let users = [
+//
+//     {
+//         id: 1,
+//         name: 'Dima',
+//         age: 32
+//     },
+//     {
+//         id: 2,
+//         name: 'Sam',
+//         age: 45
+//     },
+//     {
+//         id: 3,
+//         name: 'Igor',
+//         age: 10
+//     },
+//     {
+//         id: 4,
+//         name: 'Vania',
+//         age: 29
+//     },
+//     {
+//         id: 5,
+//         name: 'Ola',
+//         age: 95
+//     }
+// ];
+//
+// function objectArray (arr) {
+//     for (const arrElement of arr) {
+//         document.write(`<div>`)
+//         document.write(`<p>${arrElement.id}.name : ${arrElement.name}. age - ${arrElement.age}</p>`)
+//         document.write(`</div>`)
+//     }
+// }
+// objectArray(users);
+
+
 
 
 
