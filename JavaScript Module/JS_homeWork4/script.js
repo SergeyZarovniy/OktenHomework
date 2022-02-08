77255dd2a1cacc4aec87cd95dee17127b10cd260
Zarovnii Serghei <Zarovniyserhiy1991@gmail.com>
2022-02-08 12:31:57 +0100

    Homework in progress

--- JavaScript Module/JS_homeWork4/script.js
+++ JavaScript Module/JS_homeWork4/script.js
@@ -1,1 +1,51 @@
-console.log('Hello')
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+//
+// function Sumator (a,b) {
+//     let answer = a * b;
+//     return answer
+// }
+// console.log(Sumator(100,500));
+
+
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+// let arr = ['JavaScript','CSS','HTML',5666,47778,253,true];
+//
+// function ArrayLoop (arr) {
+//     for (const arrElement of arr) {
+//         console.log(arrElement);
+//     }
+// }
+// ArrayLoop(arr);
+
+
+
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+
+// function ParagraphCreator (text) {
+//     document.write(`<p>${text}</p>`);
+// }
+// ParagraphCreator('Hello world')
+
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+
+// function ListCreator (text) {
+//     document.write
+//     (`<ul>
+//          <li>${text}</li>
+//          <li>${text}</li>
+//          <li>${text}</li>
+//      </ul>`)
+// };
+// ListCreator('Hello');
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+//     Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+
+
+
+
+
