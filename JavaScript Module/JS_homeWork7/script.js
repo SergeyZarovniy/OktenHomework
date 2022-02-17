f51b5223c5fee021cca4c29d28aa770864cfa197
Zarovnii Serghei <Zarovniyserhiy1991@gmail.com>
2022-02-17 11:21:08 +0100

    Start homeWork from lesson7

---
+++ JavaScript Module/JS_homeWork7/script.js
@@ -0,0 +1,23 @@
+// - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
+// створити пустий масив, наповнити його 10 об'єктами new User(....)
+
+// function User  (id,name,surname,email,phone) {
+//     this.id = id;
+//     this.name = name;
+//     this.surname = surname;
+//     this.email = email;
+//     this.phone = phone;
+// }
+//
+// let userArray = [
+//     new User(1,'Sergei','Zarovnyi','fafa@gmail.com',556669887),
+//     new User(2,'Yana','Zarovna','bebe@gmail.com',3226568),
+//     new User(3,'Stepan','Pupkin','tetete@gmail.com',5446566),
+//     new User(4,'Roma','Angush','yayay@mail.com',455546),
+//     new User(5,'Ira','Stepanenko','vfvfvfw@gmail.com',65488648468484),
+//     new User(6,'Vasia','Streper','wevfbw@gmail.com',65488484),
+//     new User(7,'Gosia','Dluga','ththw@gmail.com',654855588874),
+//     new User(8,'Bartek','Kachur','whtyyyw@gmail.com',6544448468484),
+//     new User(9,'Adam','Nowicky','kilkow@gmail.com',102323333484),
+//     new User(10,'Severyn','Glushko','mjkl@gmail.com',25782787)
+// ];
