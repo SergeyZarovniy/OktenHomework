967d22e245512079dcccf725bc76897c8f481d08
Zarovnii Serghei <Zarovniyserhiy1991@gmail.com>
2022-02-17 12:38:58 +0100

    classWork in progress

--- JavaScript Module/JS_homeWork7/script.js
+++ JavaScript Module/JS_homeWork7/script.js
@@ -78,3 +78,46 @@
 
 //ClassWork//
 
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+
+// function Car (model,manufacturer,year,maxSpeed,engineCapacity) {
+//     this.model = model
+//     this.manufacturer = manufacturer
+//     this.year = year
+//     this.maxSpeed = maxSpeed
+//     this.engineCapacity = engineCapacity
+//     this.drive = function () {
+//          console.log(`Їдемо зі швидкістю ${this.maxSpeed} на годину`)
+//     }
+//     this.info = function () {
+//         return console.log(`
+//          Model --- ${this.model},
+//          Manufacturer --- ${this.manufacturer} ,
+//          Year --- ${this.year},
+//          Max Speed --- ${this.maxSpeed} ,
+//          Engine Capacity --- ${this.engineCapacity}`)
+//     }
+//     this.increaseMaxSpeed = function (newSpeed) {
+//         this.maxSpeed = this.maxSpeed+ newSpeed
+//     }
+//     this.changeYear = function (newValue) {
+//         console.log(` Рік машини змінено на ${this.year = newValue}`)
+//     }
+//     this.addDriver = function (driver) {
+//        this.driver = driver
+//     }
+// }
+// let car1 = new Car('Mazda','MAZDA',2006,230,2.0)
+//
+// console.log(car1);
+// car1.drive();
+// car1.info();
+// car1.increaseMaxSpeed(450)
+// car1.changeYear(2022)
+// car1.addDriver('Sergei')
