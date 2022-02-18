9e23e7f62208b336f9a63f566e7a8dda5d6aa762
Zarovnii Serghei <Zarovniyserhiy1991@gmail.com>
2022-02-18 11:48:31 +0100

    continue classWork

--- JavaScript Module/JS_homeWork7/script.js
+++ JavaScript Module/JS_homeWork7/script.js
@@ -121,3 +121,53 @@
 // car1.increaseMaxSpeed(450)
 // car1.changeYear(2022)
 // car1.addDriver('Sergei')
+
+
+
+// - (Те саме, тільки через клас)
+// Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+
+class Car {
+    constructor(model,manufacturer,year,maxSpeed,engineCapacity) {
+        this.model = model
+        this.manufacturer = manufacturer
+        this.year = year
+        this.maxSpeed = maxSpeed
+        this.engineCapacity = engineCapacity
+        this.drive = function () {
+            console.log(`їдемо зі швидкістю ${maxSpeed} на годину`)
+        }
+        this.info = function () {
+            console.log(`
+            Model:${model}
+            Manufacturer:${manufacturer}
+            Year:${year}
+            Max Speed:${maxSpeed}
+            Engine Capacity:${engineCapacity}`)
+        }
+        this.increaseMaxSpeed = function (newSpeed) {
+            this.maxSpeed = maxSpeed + newSpeed
+        }
+        this.changeYear = function (newValue) {
+            this.year = newValue
+        }
+        this.addDriver = function (driver) {
+            this.driver = driver
+        }
+    }
+}
+
+let mazdaCar = new Car('Mazda3','MAZDA',2006,230,2.0)
+console.log(mazdaCar);
+mazdaCar.drive();
+mazdaCar.info();
+mazdaCar.increaseMaxSpeed(500);
+mazdaCar.changeYear(2000);
+mazdaCar.addDriver('Zarovnii Serghei')
+console.log(mazdaCar);
